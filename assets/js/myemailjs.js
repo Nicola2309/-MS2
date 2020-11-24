8a1c23fd5edc27c8f60c08cbaafbb638d2c4509f
Nicola2309 <nicola.spigaglia@outlook.it>
2020-11-24 16:09:47 +0000

    Formatted code

--- assets/js/myemailjs.js
+++ assets/js/myemailjs.js
@@ -1,15 +1,15 @@
 function sendMail(contactForm) {
-    emailjs.send("service_u488se7","template_icii652", {
-        "from_email" : contactForm.email.value,
-        "to_name" : "Nicola",
-        "from_name" : contactForm.name.value,
-        "infos_message" : contactForm.info.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function (error) {
-            console.log("FAILED", error);
-        });
+	emailjs.send("service_u488se7", "template_icii652", {
+			"from_email": contactForm.email.value,
+			"to_name": "Nicola",
+			"from_name": contactForm.name.value,
+			"infos_message": contactForm.info.value
+		})
+		.then(
+			function (response) {
+				console.log("SUCCESS", response);
+			},
+			function (error) {
+				console.log("FAILED", error);
+			});
 }
